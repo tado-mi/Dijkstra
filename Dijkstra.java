50f80f526fc965bb0f6762296840fa14ebd4de96
tado-mi <tadouchiha@gmail.com>
2019-05-04 15:21:36 -0400

    first commit

---
+++ Dijkstra.java
@@ -0,0 +1,170 @@
+import java.util.*;
+import java.io.*;
+
+public class Dijkstra {
+
+  protected Graph graph;
+  protected String S, T, logFilename;
+
+  protected String progressLog;
+  protected boolean recordLog;
+
+  public Dijkstra(Graph graph, String filename, boolean recordLog) {
+
+    this.graph = graph;
+    this.logFilename = filename;
+
+    this.recordLog = recordLog;
+
+  }
+
+  protected void log(String str) {
+
+    progressLog += str;
+    progressLog += '\n';
+
+    recordLog(str + '\n');
+
+  }
+
+  public LinkedList<Vertex> computeShortestPath(String S, String T) {
+
+    this.S = S;
+    this.T = T;
+
+    this.progressLog = "";
+
+    long start = System.nanoTime();
+    log("m\t" + "computing path from " + S + " to " + T);
+    LinkedList<Vertex> ans = computeShortestPath();
+
+    long end = System.nanoTime();
+    long elapsed = end - start;
+    elapsed = (long) (elapsed / Math.pow(10, 6));
+    log("m\t" + "time: " + elapsed + " miliseconds.");
+
+    return ans;
+
+  }
+
+  public void init() {
+
+    long start = System.nanoTime();
+
+    for (String name: graph.vertexList()) {
+
+      Vertex v = graph.get(name);
+      v.setDistance(Double.MAX_VALUE);
+      v.unExplore();
+      v.removePath();
+
+    }
+
+    long end = System.nanoTime();
+    long elapsed = end - start;
+    elapsed = (long) (elapsed / Math.pow(10, 6));
+    log("m\t" + "initialisation complete in " + elapsed + " miliseconds.");
+
+  }
+
+  public LinkedList<Vertex> computeShortestPath() {
+
+    init();
+
+    Vertex s = graph.get(S);
+    Vertex t = graph.get(T);
+
+    if (s == null) {
+
+      log("m\t" + "source is invalid -->> terminating.");
+      return null;
+
+    }
+
+    if (t == null) {
+
+      log("m\t" + "destination is invalid -->> terminating.");
+      return null;
+
+    }
+
+    LinkedList<Vertex> shortestPath = new LinkedList<>();
+
+    s.setDistance(0.0);
+
+    PriorityQueue<Vertex> Q = new PriorityQueue<>();
+    Q.offer(s);
+
+    while(!Q.isEmpty()) {
+
+      Vertex focus = Q.poll();
+      focus.doExplore();
+
+      log("v\t" + focus + "\t" + focus.distance() + "\t" + focus.getPath());
+
+      if (focus.isEqual(t)) {
+
+        shortestPath = t.getPath();
+        log("m\t" + "-----------------------");
+        log("m\t" + "Dijkstra's algorithm is terminated with success.");
+        log("m\t" + shortestPath.toString());
+
+        return shortestPath;
+
+      }
+
+      for (Edge e: focus.adjList()) {
+
+        Vertex w = e.v;
+        if (w.isExplored()) continue;
+
+        log("c\t" + w + "\t" +"inf" + "\t" + w.getPath().toString());
+
+        double newDistance = focus.distance() + e.weight();
+        if (newDistance < w.distance()) {
+
+          w.setDistance(newDistance);
+          w.setPrevious(focus);
+          log("u\t" + w + "\t" + w.distance() + "\t" + w.getPath().toString());
+
+          Q.offer(w);
+          log("q\t" + Q.toString());
+
+        }
+
+      }
+
+    }
+
+    log("m\t" + "there is no " + s + " - " + t + " path in the graph.");
+    return null;
+
+  }
+
+  public void printLog() {
+
+    System.out.println(progressLog);
+
+  }
+
+  public void recordLog(String line) {
+
+    if (!recordLog) return;
+
+    try {
+
+      BufferedWriter wr = new BufferedWriter(new FileWriter(logFilename, true));
+      wr.write(line);
+      wr.flush();
+      wr.close();
+
+    } catch (IOException exp) {
+
+      System.out.println(exp.getMessage());
+
+    }
+
+  }
+
+
+}
