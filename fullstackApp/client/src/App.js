1c867e671d46d879a6cb97ae64e51060878c3236
DMH <dmunoz2@u.rochester.edu>
2019-10-12 19:54:50 -0400

    webApp Setup, needs webGL integration

---
+++ fullstackApp/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import WebGL from './components/WebGL'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WebGL></WebGL>
+    </div>
+  );
+}
+
+export default App;
